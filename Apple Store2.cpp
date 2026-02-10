29d92af905520eed16c57c1966716470fb72f0b2
jameson-codes <carringj7953@student.faytechcc.edu>
2026-02-10 23:19:33 +0000

    Finished M2T1

--- Apple Store2.cpp
+++ Apple Store2.cpp
@@ -5,6 +5,7 @@
 // We're going to make the simplest possible
 // "checkout" machine.
 #include <iostream>
+#include <iomanip> // For the two decimal places.
 using namespace std;
 
 
@@ -13,7 +14,8 @@ int main()  {
     //Set up all variables
     string first_name, last_name, full_name; // holds customer name
     string product = "watermelon"; // change to whatever you like
-    double cost_each = 0.99;
+    int amount_purchased;
+    double cost_each = 3.00;
     double total_cost;
 
 
@@ -27,11 +29,19 @@ int main()  {
     cout << "Nice to meet you, " << full_name << endl;
 
     // Ask how much they'd like to purchase
+    cout << "How many " << product << " do you want to buy? ";
+    cin >> amount_purchased;
 
     // Calculate total price
+    total_cost = amount_purchased * cost_each;
+
+    // Formatting: Set all prices to 2 decimal places
+    cout << setprecision(2) << fixed;
+    
     // Give the result
-
-
+    cout << "for " << amount_purchased << " " << product << endl;
+    cout << "That will be: $" << total_cost << endl;
+    cout << "Thank you for shopping with us!" << endl;
 
     return 0; // no errors
 }
