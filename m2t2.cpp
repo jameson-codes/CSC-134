22b15016f38adf5fd8f86dcbab88db0f4d2089eb
jameson-codes <carringj7953@student.faytechcc.edu>
2026-02-20 02:59:47 +0000

    started m2t2

---
+++ m2t2.cpp
@@ -0,0 +1,16 @@
+/*
+CSC 134
+M2T2 - Receipt Calculator
+jamesonc
+2/19/26
+Goal: A correct looking receipt that handles sales tax.
+Assumption: Sales tax is 7.5%
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0; // no errors
+}
